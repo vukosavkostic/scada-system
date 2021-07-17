1dad62c871fd03fa9e04085cd671aeb74f6741c3
vukosavkostic <vukosavkostic48@gmail.com>
2021-07-17 16:33:55 +0200

    added output class

---
+++ DataConcentrator/Output.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataConcentrator
+{
+    public class Output : Tag
+    {
+        public enum IO_ADDRESS
+        {
+
+        }
+
+        #region Fields
+        private IO_ADDRESS ioAddress;
+        private double initialValue;
+        #endregion
+
+        #region Properties
+        public IO_ADDRESS IOAddress
+        {
+            get
+            {
+                return ioAddress;
+            }
+
+            set
+            {
+                ioAddress = value;
+            }
+        }
+
+        public double InitialValue
+        {
+            get
+            {
+                return initialValue;
+            }
+
+            set
+            {
+                initialValue = value;
+            }
+        }
+        #endregion
+    }
+}
