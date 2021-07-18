95ec2cd09266b623abaaf90b8a822b3f31fdc857
vukosavkostic <vukosavkostic48@gmail.com>
2021-07-18 18:33:24 +0200

    added new fields

--- DataConcentrator/Analog/AnalogInput.cs
+++ DataConcentrator/Analog/AnalogInput.cs
@@ -2,15 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DataConcentrator.Analog
 {
+    public delegate void ValueHandler(string name);
+
     public class AnalogInput : Input 
     {
+        public enum AnalogInputStatus
+        {
+            REGULAR,
+            ALARMING
+        }
+
+
         #region Fields
         private List<Alarm> alarms;
+        private Thread AThread;
         private string unit;
+        private AnalogInputStatus status;
+        // Event triggers when a value is above or below critical level
+        public ValueHandler ValueChangedToCritical;
+
+        private object locker = new object();
         #endregion
 
         #region Properties
@@ -41,9 +57,40 @@ namespace DataConcentrator.Analog
                 OnPropertyChanged("Unit");
             }
         }
+
+        public AnalogInputStatus Status
+        {
+            get
+            {
+                return status;
+            }
+
+            set
+            {
+                status = value;
+                OnPropertyChanged("Status");
+            }
+        }
         #endregion
 
+        #region Methods
+        public AnalogInput()
+        {
+            this.Alarms = new List<Alarm>();
+            AThread = new Thread(ScanPLCAnalogInput);
+            this.Status = AnalogInputStatus.REGULAR;
+        }
 
+
+        public void ScanPLCAnalogInput(object obj)
+        {
+            while(true)
+            {
+
+
+            }
+        }
+        #endregion
 
     }
 }
