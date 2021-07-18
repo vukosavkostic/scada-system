17d04d6582e0c95153016611c9bb6f5f52fccdaa
vukosavkostic <vukosavkostic48@gmail.com>
2021-07-18 18:44:47 +0200

    added PLC context class

---
+++ DataConcentrator/PLCContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PLCSimulator;
+
+namespace DataConcentrator
+{
+    public class PLCContext
+    {
+        public static PLCSimulatorManager instance;
+
+        public static PLCSimulatorManager Instance
+        {
+            get
+            {
+                if(instance == null)
+                {
+                    instance = new PLCSimulatorManager();
+                    instance.StartPLCSimulator();
+                }
+
+                return instance;
+            }
+
+        }
+
+        public void StopPLCSimulator()
+        {
+            instance.Abort();
+        }
+    }
+}
