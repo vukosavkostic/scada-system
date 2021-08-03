464bb6ac3cdb4b3bfb6a6779f0b5807687a0c603
vukosavkostic <vukosavkostic48@gmail.com>
2021-08-03 17:36:15 +0200

    installed entity to ScadaGui, added reference to DataConcentrator from ScadaGui and added add tag window

--- ScadaGUI/MainWindow.xaml.cs
+++ ScadaGUI/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DataConcentrator;
+using DataConcentrator.Analog;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,16 +15,31 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace ScadaGUI
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+
+
     public partial class MainWindow : Window
     {
+        public static ScadaContext Context { get; set; }
+        public Alarm SelectedAlarm { get; set; }
+        public AnalogInput SelectedAnalogInput { get; set; }
+
+    
+
         public MainWindow()
         {
             InitializeComponent();
+
+            if (Context == null)
+            {
+                Context = new ScadaContext();
+            }
         }
 
 
@@ -33,7 +50,7 @@ namespace ScadaGUI
 
         private void ExitMainWindow(object sender, RoutedEventArgs e)
         {
-
+            Context.Dispose();
         }
 
         private void AddAlarm(object sender, RoutedEventArgs e)
